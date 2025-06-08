bdd4ca5f595623daf0c5260f70a5da48d49fb0a7
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2025-06-08 15:52:17 -0400

    feat: configure custom domains and certificates for Azure Container App

--- AppHost/AppHost.cs
+++ AppHost/AppHost.cs
@@ -5,6 +5,11 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+var apexDomain = builder.AddParameter("domainApex");
+var wwwDomain = builder.AddParameter("domainWww");
+var certName = builder.AddParameter("certificateName");
+var wwwCertName = builder.AddParameter("wwwCertificateName");
+
 // NOTE: The type returned by AddAzureStorage is IResourceBuilder<AzureStorageResource>,
 // but if storage is being cast or wrapped, ensure it is of the correct type for extension methods.
 var storage = builder.AddAzureStorage("azure-storage")
@@ -22,6 +27,13 @@ var tables = storage.AddTables("tables");
 var web = builder.AddProject<Web>("web")
 	.WithReference(tables)
 	.WaitFor(tables)
-	.WithExternalHttpEndpoints();
+	.WithExternalHttpEndpoints()
+	.PublishAsAzureContainerApp((mod,app) =>
+	{
+#pragma warning disable ASPIREACADOMAINS001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+		app.ConfigureCustomDomain(apexDomain, certName);
+		app.ConfigureCustomDomain(wwwDomain, wwwCertName);
+#pragma warning restore ASPIREACADOMAINS001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+	});
 
 builder.Build().Run();
